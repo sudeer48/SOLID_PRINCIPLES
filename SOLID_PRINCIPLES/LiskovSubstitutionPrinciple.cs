e1f227a8215e20fd3a4f8a943a39f9876d2ca5c7
sudeer48 <sudeer0408@gmail.com>
2023-03-10 12:09:17 +0530

    Add project files.

---
+++ SOLID_PRINCIPLES/LiskovSubstitutionPrinciple.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID_PRINCIPLES
+{
+    public class LiskovSubstitutionPrinciple
+    {
+
+    }
+
+    //Below are the regular form of declarations.
+
+    //public class SumCalculator
+    //{
+    //    protected readonly int[] _numbers;
+
+    //    public SumCalculator(int[] numbers)
+    //    {
+    //        _numbers = numbers;
+    //    }
+
+    //    public int Calculate() => _numbers.Sum();
+    //}
+
+    //public class EvenNumbersSumCalculator : SumCalculator
+    //{
+    //    public EvenNumbersSumCalculator(int[] numbers)
+    //        : base(numbers)
+    //    {
+    //    }
+
+    //    public new int Calculate() => _numbers.Where(x => x % 2 == 0).Sum();
+    //}
+
+    public abstract class Calculator
+    {
+        protected readonly int[] _numbers;
+
+        public Calculator(int[] numbers)
+        {
+            _numbers = numbers;
+        }
+
+        public abstract int Calculate();
+    }
+
+    public class SumCalculator : Calculator
+    {
+        public SumCalculator(int[] numbers)
+            : base(numbers)
+        {
+        }
+
+        public override int Calculate() => _numbers.Sum();
+    }
+
+    public class EvenNumbersSumCalculator : Calculator
+    {
+        public EvenNumbersSumCalculator(int[] numbers)
+           : base(numbers)
+        {
+        }
+
+        public override int Calculate() => _numbers.Where(x => x % 2 == 0).Sum();
+    }
+}
---
+++ SOLID_PRINCIPLES/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace SOLID_PRINCIPLES
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Below are the regular form of declarations.
+
+            //var numbers = new int[] { 5, 7, 9, 8, 1, 6, 4 };
+            //SumCalculator sum = new SumCalculator(numbers);
+            //Console.WriteLine($"The sum of all the numbers: {sum.Calculate()}");
+            //Console.WriteLine();
+            //EvenNumbersSumCalculator evenSum = new EvenNumbersSumCalculator(numbers);
+            //Console.WriteLine($"The sum of all the even numbers: {evenSum.Calculate()}");
+
+
+            var numbers = new int[] { 5, 7, 9, 8, 1, 6, 4 };
+            Calculator sum = new SumCalculator(numbers);
+            Console.WriteLine($"The sum of all the numbers: {sum.Calculate()}");
+            Console.WriteLine();
+            Calculator evenSum = new EvenNumbersSumCalculator(numbers);
+            Console.WriteLine($"The sum of all the even numbers: {evenSum.Calculate()}");
+        }
+    }
+}
